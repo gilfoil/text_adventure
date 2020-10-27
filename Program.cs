d1b6a9e333e66e557895458fdb64d85573b60de2
Marcus <itsgilfoil@gmail.com>
2020-10-27 13:15:30 +0100

    Beispiel

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TextAdventure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Beispiel!");
         }
     }
 }
