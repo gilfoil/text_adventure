b9d2ce4725d75508e0aa6d81826d8acabad57164
Marcus <itsgilfoil@gmail.com>
2020-10-28 10:00:39 +0100

    add Hero class

---
+++ Hero.cs
@@ -0,0 +1,170 @@
+using static System.Random;
+
+namespace TextAdventure
+{
+    public class Hero
+    {
+        private const int HEALTH_BASE = 50;
+
+        private const int MIN_ROLL = 10;
+        private const int MAX_ROLL = 20;
+
+        private const int BASE_UPGRADE_COST = 100;
+        private string name;
+        private int currentHealth;
+        private int maxHealth;
+        private int strength;
+        private int dexterity;
+        private int intelligence;
+        private int charisma;
+
+        private int coins;
+
+        private int[] upgrades;
+
+        public Hero(string heroName)
+        {
+            this.name = heroName;
+            rollStats();
+            setMaxHealth();
+            this.currentHealth = this.maxHealth;
+            this.upgrades = new int[] {1,1,1,1};
+       }
+
+        public void rollStats()
+        {
+            this.strength = Program.roll(MIN_ROLL, MAX_ROLL);
+            this.dexterity = Program.roll(MIN_ROLL, MAX_ROLL);
+            this.intelligence = Program.roll(MIN_ROLL, MAX_ROLL);
+            this.charisma = Program.roll(MIN_ROLL, MAX_ROLL);
+            this.coins = ((this.intelligence + this.charisma) / 2);
+        }
+
+        public void setMaxHealth()
+        {
+            this.maxHealth = HEALTH_BASE + (3 * strength) + (2 * dexterity);
+        }
+
+        public int getMaxHealth()
+        {
+            return this.maxHealth;
+        }
+
+        public override string ToString()
+        {
+            return $"|Name: {this.name}|\n|HP: {this.currentHealth}/{this.maxHealth}|\n|$$$: {this.coins}|\n|Strength: {this.strength}|\n|Dexterity: {this.dexterity}|\n|Intelligence: {this.intelligence}|\n|Charisma: {this.charisma}|\n";
+        }
+
+        public int getCurrentHealth() => this.currentHealth;
+        public int getStrenght() => this.strength;
+        public int getDexterity() => this.dexterity;
+        public int getIntelligence() => this.intelligence;
+        public int getCharisma() => this.charisma;
+
+        public void takeDamage(int damage)
+        {
+            if(this.currentHealth <= damage)
+            {
+                this.currentHealth = 0;
+                //game over
+                // implement me :)
+            }
+            else
+            {
+                this.currentHealth -= damage;
+            }
+        }
+
+        public void healDamage(int heal)
+        {
+            if((currentHealth + heal) > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+            else
+            {
+                currentHealth += heal;
+            }
+        }
+
+        public int getCoints() => this.coins;
+
+        public bool changeBank(int coins)
+        {
+
+            if((this.coins + coins) < 0)
+            {
+                return false;
+            }
+            else
+            {
+                this.coins += coins;
+                return true;
+            }
+        }
+
+        public void changeStat(string stat)
+        {
+            switch (stat)
+            {
+                case "str":
+                    if(changeBank(upgrades[0] * BASE_UPGRADE_COST))
+                    {
+                        this.strength += 1;
+                        setMaxHealth();
+                        upgrades[0] += 1;
+                    }
+                    else
+                    {
+                        //Error Message
+                        //Implement me :)
+                    }
+                    break;
+                case "dex":
+                    if(changeBank(upgrades[1] * BASE_UPGRADE_COST))
+                    {
+                        this.dexterity += 1;
+                        setMaxHealth();
+                        upgrades[1] += 1;
+                    }
+                    else
+                    {
+                        //Error Message
+                        //Implement me :)
+                    }
+                    break;
+                case "int":
+                    if(changeBank(upgrades[2] * BASE_UPGRADE_COST))
+                    {
+                        this.intelligence += 1;
+                        upgrades[2] += 1;
+                    }
+                    else
+                    {
+                        //Error Message
+                        //Implement me :)
+                    }
+                    break;
+                case "cha":
+                    if(changeBank(upgrades[3] * BASE_UPGRADE_COST))
+                    {
+                        this.charisma += 1;
+                        upgrades[3] += 1;
+                    }
+                    else
+                    {
+                        //Error Message
+                        //Implement me :)
+                    }
+                    break;
+                default:
+                    // Error Message
+                    // Implement me :)
+                    break;
+            }
+            
+        }
+
+        
+    }
+}
